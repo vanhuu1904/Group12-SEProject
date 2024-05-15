71cced2190e278e4199bc8ffb22198f8ca37b0b1
Van Huu <huynam569@gmail.com>
2024-05-16 03:05:37 +0700

    feat: 🎸 BE/ update api saleData/order

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -4,6 +4,7 @@ import {
   deleteOrder,
   getAllOrders,
   getOrderDetails,
+  getSales,
   myOrders,
   newOrder,
   updateOrder,
@@ -14,6 +15,10 @@ const router = express.Router();
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
 router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
 router.route("/me/orders").get(isAuthenticatedUser, myOrders);
+
+router
+  .route("/admin/get_sales")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSales);
 
 router
   .route("/admin/orders")
