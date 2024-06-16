e5a37ab9863fa5ff27d96b86d04ae48c2dcb71ce
Van Huu <huynam569@gmail.com>
2024-06-16 21:55:20 +0700

    feat: update db/product.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import cors from "cors";
 import path from "path";
 // import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
+console.log("filename: " + __filename);
 const __dirname = path.dirname(__filename);
 
 // Handle Uncaught exceptions
